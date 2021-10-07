cdbe43737a4fc64a72e2f89b4ba468ab5c8d6541
AdriAlvarez00 <adralv01@ucm.es>
2021-10-07 16:15:40 +0200

    Limpieza y gitignore

--- P1/practica1.py
+++ P1/practica1.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
-from matplotlib.ticker import LinearLocator
 
 
 def carga_csv(filename):

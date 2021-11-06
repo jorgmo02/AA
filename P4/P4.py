ed3323ccb2059a90395afe5859efb643120d66be
Jorge Moreno Martínez <jorgmo02@ucm.es>
2021-11-06 19:41:10 +0100

    avances backprop
    Co-Authored-By: Adrián Álvarez <48672128+AdriAlvarez00@users.noreply.github.com>

--- P4/P4.py
+++ P4/P4.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
 from scipy import optimize
+import checkNNGradients
 
 def load_data(file='ex4data1.mat'):
     data = loadmat(file)
@@ -55,12 +56,45 @@ def coste_red_regularizado(X, Y, Theta1, Theta2, reg):
 
     return coste_sin_regularizar + reg/(2*m) * (a+b)
 
+
+def backprop(params_rn, num_entradas, num_ocultas, num_etiquetas, X, y, reg):
+    
+    m = X.shape[0]
+
+    Theta1 = np.reshape(params_rn[:num_ocultas * (num_entradas + 1) ], (num_ocultas, (num_entradas + 1)))
+    Theta2 = np.reshape(params_rn[ num_ocultas * (num_entradas + 1):], (num_etiquetas, (num_ocultas + 1)))
+
+    Delta1 = np.zeros(Theta1.shape)
+    Delta2 = np.zeros(Theta2.shape)
+
+    print(Delta1.shape)
+    print(Delta2.shape)
+
+    A1, A2, h = propaga_red(X, Theta1, Theta2)
+
+    coste = 0
+
+    for k in range(m):
+        a1k = A1[k, :]  # (401,)
+        a2k = A2[k, :]  # (26,)
+        hk = h[k, :]    # num neuronas ultima capa
+        yk = y[k]
+
+        d3k = hk - yk   # error (10,)
+        d2k = np.dot(Theta2.T, d3k) * (a2k * (1 - a2k)) # (26,)
+
+
+
 def main():
     weights = loadmat('ex4weights.mat')
     Theta1, Theta2 = weights['Theta1'], weights['Theta2']
     X, y = load_data()
     print(coste_red(X, y,Theta1, Theta2))
     print(coste_red_regularizado(X, y, Theta1, Theta2, 1))
+    print(Theta1.shape)
+    print(Theta2.shape)
+    # TODO sacar los tamaños limpios
+    backprop(np.concatenate([Theta1.ravel(), Theta2.ravel()]), Theta1.shape[1]-1, Theta2.shape[1]-1, 10, X, y, 1)
 
 
 main()

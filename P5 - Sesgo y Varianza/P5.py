7564795a704324f0a7b0996734bea0168dd31f07
AdriAlvarez00 <adralv01@ucm.es>
2021-11-25 17:43:06 +0100

    Changes by usuario_local

--- P5 - Sesgo y Varianza/P5.py
+++ P5 - Sesgo y Varianza/P5.py
@@ -86,6 +86,87 @@ def minimize_this_pls(Theta, X, Y, lamb):
     return coste_regularizado(Theta, X, Y, lamb), gradiente_regularizado(Theta, X, Y, lamb)
 
 
+def normaliza_matriz(x):
+    mu = np.mean(x, axis=0)  # Media de cada columna
+    sigma = np.std(x, axis=0)  # Desviacion estandar por columnas, no confundir con la querida std de c++
+
+    x_norm = (x - mu) / sigma
+
+    return x_norm, mu, sigma
+
+
+def polinomiza_atributos(X, p):
+    Pol = X
+    for i in range(2, p + 1):
+        Pol = np.hstack([Pol, X ** i])
+
+    return Pol
+
+
+def load_data(filename='ex5data1.mat'):
+    data = loadmat(filename)
+    X = data['X']
+    Y = data['y']
+    Y = Y[:, -1]
+    X_val = data['Xval']
+    Y_val = data['yval']
+    Y_val = Y_val[:, -1]
+    X_test = data['Xtest']
+    Y_test = data['ytest']
+    Y_test = Y_test[:, -1]
+    return X, Y, X_val, Y_val, X_test, Y_test
+
+
+def learning_curves():
+    X, Y, X_val, Y_val, X_test, Y_test = load_data()
+
+    X = np.hstack([np.ones([X.shape[0], 1]), X])
+    X_val = np.hstack([np.ones([X_val.shape[0], 1]), X_val])
+    m = X.shape[0]
+
+    Errors = np.empty((m, 2))
+    sliceSize = np.arange(1, m)
+
+    for i in sliceSize:
+        Theta = np.array([1, 1])
+        res = scipy.optimize.minimize(minimize_this_pls, Theta, args=(X[0:i], Y[0:i], 0),
+                                      jac=True, method='TNC')
+        err = coste_lineal(res.x, X[0:i], Y[0:i])
+        errVal = coste_lineal(res.x, X_val, Y_val)
+        Errors[i - 1] = np.array([err, errVal])
+
+    plot_line(sliceSize, Errors[:-1, 0])
+    plot_line(sliceSize, Errors[:-1, 1])
+    plt.show()
+
+
+def try_funcs():
+    X, Y, X_val, Y_val, X_test, Y_test = load_data()
+    m = X.shape[0]
+    Theta = np.array([1, 1])
+    X = np.hstack([np.ones([X.shape[0], 1]), X])
+
+    print(coste_regularizado(Theta, X, Y, 1))
+    print(gradiente_regularizado(Theta, X, Y, 1))
+
+
+def polynomial_regression():
+    X, Y, X_val, Y_val, X_test, Y_test = load_data()
+    m = X.shape[0]
+    p = 8
+    Pol_X = polinomiza_atributos(X, p)
+    Pol_X = np.hstack([np.ones([Pol_X.shape[0], 1]), Pol_X])
+    Norm_Pol_X, media, varianza = normaliza_matriz(Pol_X)
+    Theta = np.ones(Norm_Pol_X.shape[1])
+
+    lamb = 0
+    res = scipy.optimize.minimize(minimize_this_pls, Theta, args=(Norm_Pol_X, Y, lamb),
+                                  jac=True, method='TNC')
+
+    plot_polynomial_regression(X, Y, res.x, p)
+    plt.show()
+
+
 def main():
     # plot_line(X[:, 1:], Y, res.x)
     # Errors = learning_curves(X, Y, X_val, Y_val)
